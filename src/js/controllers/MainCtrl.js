0c26ffd69cfcbe5e8ee8dae4ef5ac0ac6eeafe9b
maltempi <tmmaltempi@gmail.com>
2016-03-05 13:28:49 -0300

    Adding CartoDB on project

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -1,10 +1,14 @@
 'use strict';
 
-console.log('here i am')
 angular.module('smscApp').controller('MainCtrl', MainCtrl);
+
 
 MainCtrl.$inject = ['$routeParams'];
 
 function MainCtrl($routeParams) {
-    var mainCtrl = this;
+    this.init();
 }
+
+MainCtrl.prototype.init = function () {
+    cartodb.createVis('map', 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json');
+};
